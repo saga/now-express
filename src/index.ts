5a0e7912e7b1960c6d3cd78c29011662bd92be9d
wei <sagasw@gmail.com>
2020-03-10 17:03:40 +0800

    change

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { json, urlencoded } from "body-parser";
 import express from "express";
+import * as compression from "compression";
+import * as helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
+app.use(json());
+app.use(compression());
 app.use(urlencoded({ extended: true }));
 app.set('trust proxy', 1);
 
