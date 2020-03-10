b3d1a398e015f299f2dfc4b61206b46cce993e2d
wei <sagasw@gmail.com>
2020-03-10 16:45:06 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import { json, urlencoded } from "body-parser";
-import * as compression from "compression";
 import * as express from "express";
-import * as helmet from "helmet";
-import * as path from "path";
 
 const app = express();
 
-app.use(helmet());
-app.use(json());
-app.use(compression());
 app.use(urlencoded({ extended: true }));
 app.set('trust proxy', 1);
 
-// serve the client side
-app.use(express.static(path.join(__dirname, "../client")));
-
-app.get('/api', (req, response) => {
+app.get('/', (req, response) => {
     response.send('Hello world!');
 });
 
