143085c92c8f273da0cbf230444a9514f6bd7a5b
wei <sagasw@gmail.com>
2020-03-10 18:03:47 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,6 @@ app.use(compression());
 app.use(urlencoded({ extended: true }));
 app.set('trust proxy', 1);
 
-app.get('/', (req, response) => {
-    response.send('Hello world!');
-});
 
 const pool = mysql.createPool({
     connectionLimit: 25,
@@ -25,15 +22,23 @@ const pool = mysql.createPool({
     port: 3306
 });
 
+app.get('/', (req, response) => {
+    const timeNow = new Date();
+
+    let sql = `INSERT INTO logs(content)
+           VALUES("aabbccdd")`;
+    pool.query(sql);
+
+    response.send('Hello world!');
+});
+
+
 pool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
 });
 
-// const timeNow = new Date();
-// let sql = `INSERT INTO logs(time, content)
-//            VALUES(${timeNow}, "aabbccdd")`;
-// pool.query(sql);
+
 
 app.listen(3000, () => {
     console.log("server start to monitor 3000");
